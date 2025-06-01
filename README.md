a05faf1f480121af6ed29badfe1dc7777836beb9
Sebailla <seba.illa.1977@gmail.com>
2025-05-31 22:04:01 -0300

    chore: create seed database

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Descripción
 
+Proyecto de Ecommers, realizado en NextJs y TypeScript
 
+## Arquitectura
+
+* Swiper
+* Zustand
+* NextJs
+* Tailwind Css
+* TypeScript
+* Postgresql
+* Prisma
+* Docker
 
 ## Correr en dev
-
 
 1. Clonar el repositorio.
 2. Crear una copia del ```.env.template``` y renombrarlo a ```.env``` y cambiar las variables de entorno.
@@ -12,8 +22,5 @@
 5. Correr las migraciones de Primsa ```npx prisma migrate dev```
 6. Ejecutar seed ```npm run seed```
 7. Correr el proyecto ```npm run dev```
-
-
-
 
 ## Correr en prod
---
+++ src/seed/seed-database.ts
@@ -0,0 +1,56 @@
+import prisma from '../lib/prisma'
+import { initialData } from './seed'
+
+async function main() {
+
+    //? Clear database
+        await prisma.productImage.deleteMany()
+        await prisma.product.deleteMany()
+        await prisma.category.deleteMany()
+
+    const { categories, products } = initialData
+
+    //? Category insert
+    const categoryData = categories.map(category => ({
+        name: category
+    }))
+    await prisma.category.createMany({
+        data: categoryData
+    })
+
+    //relaciones
+    const categoriesBD = await prisma.category.findMany()
+
+    const categoriesMap = categoriesBD.reduce((map, category) => {
+        map[category.name.toLowerCase()] = category.id
+        return map
+    }, {} as Record<string, string>) //<string=name, string=categoryId>
+
+    //? Product insert
+    products.forEach(async product => {
+        const { type, images, ...rest } = product
+        
+        const dbProduct = await prisma.product.create({
+            data: {
+                ...rest,
+                categoryId: categoriesMap[type]
+            }
+        })
+
+        //? productImage insert
+        const imagesData = images.map(image => ({
+            url: image,
+            productId: dbProduct.id
+        }))
+        await prisma.productImage.createMany({
+            data: imagesData
+        })
+    })
+
+    console.log('Seed executed successfully')
+}
+
+(async () => {
+    if (process.env.NODE_ENV === 'production') return
+    await main()
+})()
